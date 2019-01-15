956e2081d69a4cbda36ba34390ceaca966997699
ppaint <pierre.paint@free.fr>
2019-01-15 16:52:26 +0100

    Creation des dossiers

---
+++ src/main/java/Apptest.java
@@ -0,0 +1,9 @@
+public class Apptest {
+
+	public static void main(String[] args) {
+		
+		Context.close();
+	}
+ 
+}
+ 
---
+++ src/main/java/util/Context.java
@@ -0,0 +1,22 @@
+package util;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Context {
+	private static EntityManagerFactory emf =null;
+	
+	public static EntityManagerFactory getEntityManagerFactory() {
+		if(emf == null) {
+			emf = Persistence.createEntityManagerFactory("JPAmaventest");
+		}
+		return emf;
+	}
+	
+	public static void close() {
+		if (emf != null) {
+			emf.close();
+			emf = null;
+		}
+		
+	}
+}
